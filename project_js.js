abd94812d176328f597e8cf4c1c28d4caf990970
lth-1026 <th20001026@gmail.com>
2020-08-25 22:25:02 +0900

    add div box

---
+++ project_js.js
@@ -0,0 +1,13 @@
+function add_div() {
+  var div = document.createElement('div');
+
+  div.innerHTML = document.getElementById('main_container').innerHTML;
+  document.getElementById('field').appendChild(div);
+  div.style.backgroundColor = 'gray';
+  div.style.margin = '10px 0px 10px 0px';
+  div.style.padding = '10px';
+}
+
+function remove_div(obj) {
+  document.getElementById('field').removeChild(obj.parentNode);
+}
