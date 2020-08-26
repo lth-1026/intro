c2d5b1fa67d25864dd31b35b089174fe3b46e51c
lth-1026 <th20001026@gmail.com>
2020-08-26 13:33:50 +0900

    add php

---
+++ education.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      <li><?php echo hi ?></li>
+    </ul>
+  </body>
+</html>
